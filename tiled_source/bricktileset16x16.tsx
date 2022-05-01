cc3a59f618a631644a84765a9435e97bc96feba2
luis.boden <luis.boden@gmail.com>
2022-05-01 22:25:50 +0200

    register

---
+++ tiled_source/bricktileset16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="bricktileset16x16" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../src/main/resources/sprite/tile/bricktileset16x16.png" trans="ff00ff" width="128" height="128"/>
+</tileset>
